e75d4e328a81e1f1fea451f12957b5971687cba5
Author985245 <Author985245@localhost.l>
2016-10-09 19:33:09 -0400

    Give the right pointer for the arp cache mac.

--- router/sr_router.c
+++ router/sr_router.c
@@ -150,7 +150,7 @@ void handle_arp(struct sr_instance* sr, uint8_t * packet, unsigned int len, char
 				if(interface_listing->ip == orig_arp->ar_dest_ip)
 				{
 					memcpy(dest_mac, interface_listing->mac, 6);
-					backlog = sr_arpcache_insert(&(sr->cache), cache_hit->mac, orig_arp->ar_dest_ip);
+					backlog = sr_arpcache_insert(&(sr->cache), dest_mac, orig_arp->ar_dest_ip);
 
 					/*only proccess the backlog if there is one. otherwise backlog->packet will give a memory read exception*/
 					if(backlog != NULL)
